b8e2b3be9bebd74d076886afa9b759732f8477ea
J Fixby <github@jfixby.com>
2019-07-03 15:05:04 +0200

    upload on different hash

--- reynen-court-demo-deploy/src/com/jfixby/reynencourt/demo/deploy/run/UploadJars.java
+++ reynen-court-demo-deploy/src/com/jfixby/reynencourt/demo/deploy/run/UploadJars.java
@@ -4,6 +4,7 @@ package com.jfixby.reynencourt.demo.deploy.run;
 import java.io.IOException;
 
 import com.jfixby.scarabei.api.file.File;
+import com.jfixby.scarabei.api.file.FileConflistResolver;
 import com.jfixby.scarabei.api.file.LocalFileSystem;
 import com.jfixby.scarabei.aws.api.s3.S3FileSystem;
 
@@ -12,8 +13,20 @@ public class UploadJars {
 	public static void main (final String[] args) throws IOException {
 		final S3FileSystem fs = Setup.setup();
 		final File bucketRoot = fs.ROOT();
+		final File destination = bucketRoot.child("jars");
 
-		LocalFileSystem.ApplicationHome().parent().child("").
+		final File source = LocalFileSystem.ApplicationHome().child("upload");
+
+		fs.copyFolderContentsToFolder(source, destination, conflictResolution);
 	}
+
+	static FileConflistResolver conflictResolution = new FileConflistResolver() {
+		@Override
+		public boolean overwrite (final File fileToCopy, final File existing) throws IOException {
+			final String newHash = fileToCopy.calculateHash().getMD5HashHexString();
+			final String oldHash = existing.calculateHash().getMD5HashHexString();
+			return !oldHash.equals(newHash);
+		}
+	};
 
 }
