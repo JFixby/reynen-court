fc8e7c68a2b68d5399bdceb4cc97c38621639894
J Fixby <github@jfixby.com>
2019-07-03 14:31:48 +0200

    configure lambda test

---
+++ reynen-court-demo-lambda/scr/com/jfixby/reynencourt/demo/RunStub.java
@@ -0,0 +1,12 @@
+
+package com.jfixby.reynencourt.demo;
+
+import com.jfixby.reynencourt.demo.lambda.ConsumeDataSample;
+
+public class RunStub {
+
+	public static void main (final String[] args) {
+		ConsumeDataSample.init();
+	}
+
+}
--- reynen-court-demo-lambda/scr/com/jfixby/reynencourt/demo/lambda/ConsumeDataSample.java
+++ reynen-court-demo-lambda/scr/com/jfixby/reynencourt/demo/lambda/ConsumeDataSample.java
@@ -1,12 +1,34 @@
 
-package com.jfixby.reynencourt.sns.demo.storage.lambda;
+package com.jfixby.reynencourt.demo.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.jfixby.reynencourt.demo.DataSample;
+import com.jfixby.scarabei.api.collections.Map;
+import com.jfixby.scarabei.api.json.Json;
+import com.jfixby.scarabei.api.log.L;
+import com.jfixby.scarabei.api.names.ID;
+import com.jfixby.scarabei.api.sys.settings.ExecutionMode;
+import com.jfixby.scarabei.api.sys.settings.SystemSettings;
+import com.jfixby.scarabei.red.desktop.ScarabeiDesktop;
 
-public class LambdaHandler implements RequestHandler<String, String> {
+public class ConsumeDataSample implements RequestHandler<DataSample, String> {
+
+	public static void init () {
+	}
+
+	static {
+		ScarabeiDesktop.deploy();
+		SystemSettings.setExecutionMode(ExecutionMode.EARLY_DEVELOPMENT);
+		final Map<ID, Object> settings = SystemSettings.listAllSettings();
+		L.d("System settings", settings);
+		System.getenv("S3_BUCKET_NAME");
+	}
+
+	static DataSample DefaultDataSample = Json.deserializeFromString(DataSample.class, "");
+
 	@Override
-	public String handleRequest (final String input, final Context context) {
+	public String handleRequest (final DataSample input, final Context context) {
 		context.getLogger().log("Input: " + input);
 		return "Output: <" + input + ">";
 	}
