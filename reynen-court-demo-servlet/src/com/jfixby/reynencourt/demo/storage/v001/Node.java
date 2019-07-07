f5f7a2e8332a8115296a403a9d0cf62e3c40a9bf
J Fixby <github@jfixby.com>
2019-07-08 00:15:08 +0200

    move out node

--- reynen-court-demo-servlet/src/com/jfixby/reynencourt/demo/storage/v001/Node.java
+++ reynen-court-demo-servlet/src/com/jfixby/reynencourt/demo/storage/v001/Node.java
@@ -1,5 +1,121 @@
+
 package com.jfixby.reynencourt.demo.storage.v001;
 
-public class Node {
+import java.util.ArrayList;
+
+import com.jfixby.scarabei.api.log.L;
+
+class Node<T> {
+	/**
+	 *
+	 */
+	private final SimpleStorageIndex Node;
+
+	/** @param simpleStorageIndex */
+	public Node (final SimpleStorageIndex simpleStorageIndex) {
+		this.Node = simpleStorageIndex;
+	}
+
+	private final ArrayList<Node<T>> left = new ArrayList<>();
+	private final ArrayList<Node<T>> right = new ArrayList<>();
+	T content;
+
+	public int maxRightLevel () {
+		return this.right.size() - 1;
+	}
+
+	public int maxLeftLevel () {
+		return this.left.size() - 1;
+	}
+
+	@Override
+	public String toString () {
+		return this.toString(0);
+	}
+
+	public String toString (final int level) {
+		String left = "<-";
+		if (this.getLeft(level) == null) {
+			left = "x-";
+		}
+		String right = "->";
+		if (this.getRight(level) == null) {
+			right = "-x";
+		}
+
+		String print = "";
+
+		if (new Integer(96).equals(this.content)) {
+			L.d();
+			this.printLevels();
+		}
+
+		if (level == 0) {
+			if (this.content != null) {
+				print = this.content + "";
+			} else {
+				print = "...";
+			}
+		} else if (level == this.maxNodeLevel()) {
+			print = "T ";
+		}
+
+		return left + String.format("%3s", print) + right;
+
+	}
+
+	int maxNodeLevel () {
+		return Math.max(this.maxLeftLevel(), this.maxRightLevel());
+	}
+
+	public void clearLeft () {
+		this.left.clear();
+
+	}
+
+	public void clearRight () {
+		this.right.clear();
+
+	}
+
+	public void addLeft (final Node<T> left) {
+		this.left.add(left);
+
+	}
+
+	public void addRight (final Node<T> right) {
+		this.right.add(right);
+
+	}
+
+	public Node<T> getRight (final int level) {
+		if (this.right.size() <= level) {
+			return null;
+		}
+		return this.right.get(level);
+	}
+
+	public Node<T> getLeft (final int level) {
+		if (this.left.size() <= level) {
+			return null;
+		}
+		return this.left.get(level);
+	}
+
+	public void setLeft (final int level, final Node<T> newNode) {
+		this.left.set(level, newNode);
+	}
+
+	public void setRight (final int level, final Node<T> newNode) {
+		this.right.set(level, newNode);
+	}
+
+	public void printLevels () {
+		for (int i = this.maxNodeLevel(); i >= 0; i--) {
+			final String l = this.getLeft(i) + "";
+			final String r = this.getRight(i) + "";
+			L.d(l + " :" + i + ": " + r);
+		}
+	}
 
 }
--- reynen-court-demo-servlet/src/com/jfixby/reynencourt/demo/storage/v001/SimpleStorageIndex.java
+++ reynen-court-demo-servlet/src/com/jfixby/reynencourt/demo/storage/v001/SimpleStorageIndex.java
@@ -2,7 +2,6 @@
 package com.jfixby.reynencourt.demo.storage.v001;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import com.jfixby.reynencourt.demo.DataSample;
 import com.jfixby.reynencourt.demo.storage.StorageIndex;
@@ -111,8 +110,8 @@ public class SimpleStorageIndex implements StorageIndex {
 
 	int size = 0;
 	int maxLevel = 0;
-	final Node<DataSample> left = new Node<>();
-	final Node<DataSample> right = new Node<>();
+	final Node<DataSample> left = new Node<>(this);
+	final Node<DataSample> right = new Node<>(this);
 
 	public void clear () {
 		this.size = 0;
@@ -120,111 +119,6 @@ public class SimpleStorageIndex implements StorageIndex {
 		this.left.clearRight();
 		this.right.clearLeft();
 		this.ensureMaxLevel();
-
-	}
-
-	class Node<T> {
-		private final ArrayList<Node<T>> left = new ArrayList<>();
-		private final ArrayList<Node<T>> right = new ArrayList<>();
-		T content;
-
-		public int maxRightLevel () {
-			return this.right.size() - 1;
-		}
-
-		public int maxLeftLevel () {
-			return this.left.size() - 1;
-		}
-
-		@Override
-		public String toString () {
-			return this.toString(0);
-		}
-
-		public String toString (final int level) {
-			String left = "<-";
-			if (this.getLeft(level) == null) {
-				left = "x-";
-			}
-			String right = "->";
-			if (this.getRight(level) == null) {
-				right = "-x";
-			}
-
-			String print = "";
-
-			if (new Integer(96).equals(this.content)) {
-				L.d();
-				this.printLevels();
-			}
-
-			if (level == 0) {
-				if (this.content != null) {
-					print = this.content + "";
-				} else {
-					print = "...";
-				}
-			} else if (level == this.maxNodeLevel()) {
-				print = "T ";
-			}
-
-			return left + String.format("%3s", print) + right;
-
-		}
-
-		private int maxNodeLevel () {
-			return Math.max(this.maxLeftLevel(), this.maxRightLevel());
-		}
-
-		public void clearLeft () {
-			this.left.clear();
-
-		}
-
-		public void clearRight () {
-			this.right.clear();
-
-		}
-
-		public void addLeft (final Node<T> left) {
-			this.left.add(left);
-
-		}
-
-		public void addRight (final Node<T> right) {
-			this.right.add(right);
-
-		}
-
-		public Node<T> getRight (final int level) {
-			if (this.right.size() <= level) {
-				return null;
-			}
-			return this.right.get(level);
-		}
-
-		public Node<T> getLeft (final int level) {
-			if (this.left.size() <= level) {
-				return null;
-			}
-			return this.left.get(level);
-		}
-
-		public void setLeft (final int level, final Node<T> newNode) {
-			this.left.set(level, newNode);
-		}
-
-		public void setRight (final int level, final Node<T> newNode) {
-			this.right.set(level, newNode);
-		}
-
-		public void printLevels () {
-			for (int i = this.maxNodeLevel(); i >= 0; i--) {
-				final String l = this.getLeft(i) + "";
-				final String r = this.getRight(i) + "";
-				L.d(l + " :" + i + ": " + r);
-			}
-		}
 
 	}
 
@@ -294,7 +188,7 @@ public class SimpleStorageIndex implements StorageIndex {
 	}
 
 	private Node<DataSample> insertNodeAfter (final Node<DataSample> current, final int level) {
-		final Node<DataSample> newNode = new Node<>();
+		final Node<DataSample> newNode = new Node<>(this);
 		newNode.addLeft(current);
 		newNode.addRight(current.getRight(level));
 		current.getRight(level).setLeft(level, newNode);
